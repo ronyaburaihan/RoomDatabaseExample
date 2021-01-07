8c5d7c7288895cc761cc2e8582cd541a9038e265
Abu Raihan Rony <44436797+ronyaburaihan@users.noreply.github.com>
2021-01-07 22:24:31 +0600

    Read data from database

---
+++ app/src/main/java/com/techdoctorbd/roomdatabaseexample/fragments/list/ListAdapter.kt
@@ -0,0 +1,42 @@
+package com.techdoctorbd.roomdatabaseexample.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.techdoctorbd.roomdatabaseexample.R
+import com.techdoctorbd.roomdatabaseexample.data.User
+
+class ListAdapter() : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+
+    private var userList = emptyList<User>()
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvNumber: TextView = itemView.findViewById(R.id.tv_number)
+        val tvFistName: TextView = itemView.findViewById(R.id.tv_first_name)
+        val tvLastName: TextView = itemView.findViewById(R.id.tv_last_name)
+        val tvAge: TextView = itemView.findViewById(R.id.tv_age)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_user_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val currentUserItem = userList[position]
+        holder.tvNumber.text = currentUserItem.id.toString()
+        holder.tvFistName.text = currentUserItem.firstName
+        holder.tvLastName.text = currentUserItem.lastName
+        holder.tvAge.text = currentUserItem.age.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    fun setData(users: List<User>) {
+        this.userList = users
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/techdoctorbd/roomdatabaseexample/fragments/list/ListFragment.kt
+++ app/src/main/java/com/techdoctorbd/roomdatabaseexample/fragments/list/ListFragment.kt
@@ -5,18 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.techdoctorbd.roomdatabaseexample.R
+import com.techdoctorbd.roomdatabaseexample.UserViewModel
 import com.techdoctorbd.roomdatabaseexample.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
@@ -28,5 +32,23 @@ class ListFragment : Fragment() {
         binding.floatingActionButtonList.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+
+        val adapter = ListAdapter()
+
+        binding.recyclerViewList.apply {
+            this.adapter = adapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+        //for UserViewModel
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        userViewModel.readAllData.observe(viewLifecycleOwner, {
+            adapter.setData(it)
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
